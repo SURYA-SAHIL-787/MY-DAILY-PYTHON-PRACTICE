eb385aa7eda2f6855ed586959b5c124badcecf42
SURYA SAHIL - 787 <254373338+SURYA-SAHIL-787@users.noreply.github.com>
2026-06-17 08:03:00 +0530

    Create range_sum_fenwick.py

---
+++ range_sum_fenwick.py
@@ -0,0 +1,28 @@
+n = int(input("Enter size of array: "))
+arr = list(map(int, input("Enter array: ").split()))
+
+fenwick = [0] * (n + 1)
+
+def update(index, value):
+    while index <= n:
+        fenwick[index] += value
+        index += index & -index
+
+def prefix_sum(index):
+    total = 0
+    while index > 0:
+        total += fenwick[index]
+        index -= index & -index
+    return total
+
+def range_sum(left, right):
+    return prefix_sum(right) - prefix_sum(left - 1)
+
+for i in range(n):
+    update(i + 1, arr[i])
+
+q = int(input("Enter number of queries: "))
+
+for _ in range(q):
+    l, r = map(int, input("Enter left and right: ").split())
+    print("Range sum:", range_sum(l, r))
