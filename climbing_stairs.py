223866c6038c5e780972c7f50156b4df60652342
SURYA SAHIL - 787 <254373338+SURYA-SAHIL-787@users.noreply.github.com>
2026-09-13 20:23:08 +0530

    Create climbing_stairs.py

---
+++ climbing_stairs.py
@@ -0,0 +1,18 @@
+def climbing_stairs(n):
+    if n <= 2:
+        return n
+
+    dp = [0] * (n + 1)
+
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
+
+
+n = 5
+
+print("Number of ways =", climbing_stairs(n))
